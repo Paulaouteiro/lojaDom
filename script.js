e89f737d5183966f84adade750d239cd6e0e5d58
“Paula <paulamouteiro@gmail.com>
2024-05-13 20:17:23 -0300

    Criação da loja

---
+++ script.js
@@ -0,0 +1,17 @@
+let elementobody = document.querySelector("body");
+console.log(elementobody);
+
+elementobody.innerHTML=`
+
+<div id= "bolsaBarata">
+<h2>Bolsa Dior</h2>
+<img src= "img/bolsaDior.png">
+<br>
+<a>Dior Book Tote média</a>
+<h3>R$5.000,00</h3>
+
+<h3> <a href="https://www.dior.com/pt_br/fashion/products/M1296ZEGG_M933-dior-book-tote-media-bordado-paris-branco-e-preto-36-x-27-5-x-16-5-cm"> Comprar </a></li>
+
+
+`
+
